c7f39e30e5de53acdb2b26be7f2a4e378017ad4e
skjha336-dotcom <skjha336@gmail.com>
2026-01-09 22:55:04 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,24 @@
+output "s3_bucket_name" {
+  value = aws_s3_bucket.csv.bucket
+}
+
+output "lambda_function_name" {
+  value = aws_lambda_function.mailer.function_name
+}
+
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private.id
+}
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.nat.id
+}
+
+output "ses_sender_email_identity" {
+  value = aws_ses_email_identity.sender.email
+}
 
